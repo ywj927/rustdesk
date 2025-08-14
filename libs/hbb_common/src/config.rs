0d412cc29ec265c8bd73e7e185c2a398f9748b57
RustDesk Automation <rustdesk@example.com>
2025-08-14 04:16:28 +0000

    更新服务器配置及转换为本地模块

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["http://123927.xyz"];
+pub const RS_PUB_KEY: &str = "lnOo3Ri1DW0LKE5rpoOr2T8DjEd21B2vP8fPgVZIwcY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
